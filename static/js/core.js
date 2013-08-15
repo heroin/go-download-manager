70b56e3031c4edb4e54c561098e892a4939a2925
Heroin <heroin.nee@gmail.com>
2013-08-15 22:10:26 +0800

    parent dir link bug

--- static/js/core.js
+++ static/js/core.js
@@ -38,21 +38,30 @@ $(document).ready(function () {
 
     function make(url, name, date, dir) {
         html = "";
-        html += "                    <tr>\n";
-        html += "                      <td class=\"file-name\"item-type= \"" + (dir ? "dir" : "file") + "\" data-name=\"" + url + ((url.substring(url.length - 1) != "/" && name != "..") ? "/" : "") + (name != ".." ? name : "") + "\">";
-        if (dir) {
-            html += "<a href=\"javascript:void(0);\" class=\"load-path\">"
+        if (name == "..") {
+            html += "                    <tr>\n";
+            html += "                      <td colspan=\"3\" class=\"file-name\" item-type= \"dir\" data-name=\"\"><a href=\"javascript:void(0);\" class=\"load-path\">";
+            html += ""
+            html += name + "/";
+            html += "</a></td>\n";
+            html += "                    </tr>\n";
         } else {
-            html += "<a href=\"http://download.heroin.so" + url + (url.substring(url.length - 1) != "/" ? "/" : "" ) + name + "\" target=\"_blank\">";
+            html += "                    <tr>\n";
+            html += "                      <td class=\"file-name\" item-type= \"" + (dir ? "dir" : "file") + "\" data-name=\"" + url + (url.substring(url.length - 1) != "/" ? "/" : "") + name + "\">";
+            if (dir) {
+                html += "<a href=\"javascript:void(0);\" class=\"load-path\">"
+            } else {
+                html += "<a href=\"http://download.heroin.so" + url + (url.substring(url.length - 1) != "/" ? "/" : "" ) + name + "\" target=\"_blank\">";
+            }
+            html += name + (dir ? "/" : "");
+            html += "</a></td>\n";
+            html += "                      <td class=\"center\">" + date + "</td>\n";
+            html += "                      <td class=\"center\">\n";
+            html += "                        <a class=\"file-rename\" title=\"rename\" href=\"#win-rename-modal\" data-toggle=\"modal\"><i class=\"icon-edit\"></i></a>\n";
+            html += "                        <a class=\"file-remove\" title=\"remove\" href=\"javascript:void(0);\"><i class=\"icon-remove\"></i></a>\n";
+            html += "                      </td>\n";
+            html += "                    </tr>\n";
         }
-        html += name + (dir ? "/" : "");
-        html += "</a></td>\n";
-        html += "                      <td class=\"center\">" + date + "</td>\n";
-        html += "                      <td class=\"center\">\n";
-        html += "                        <a class=\"file-rename\" title=\"rename\" href=\"#win-rename-modal\" data-toggle=\"modal\"><i class=\"icon-edit\"></i></a>\n";
-        html += "                        <a class=\"file-remove\" title=\"remove\" href=\"javascript:void(0);\"><i class=\"icon-remove\"></i></a>\n";
-        html += "                      </td>\n";
-        html += "                    </tr>\n";
         return html;
     }
 
@@ -79,7 +88,7 @@ $(document).ready(function () {
         });
     }
 
-    load("");
+    load("/");
 
     $("#win-rename-modal input").focus(function () {
         $(this).parent().parent().removeClass("error").removeClass("success");
